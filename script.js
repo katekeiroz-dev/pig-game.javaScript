e6cd7f1c863827baa7609f7acd2e6374418e9413
kate keiroz <katekeiroz@gmail.com>
2025-02-03 21:07:09 +0000

    reset game functionallity

--- script.js
+++ script.js
@@ -13,14 +13,32 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEL.classList.add('hidden');
 
-const scores = [0,0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore ,activePlayer, playing ;
+
+
+
+// starting conditions 
+const init = function(){
+
+    scores = [0,0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEL.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+
+};
+
 
 const switchPlayer = function(){
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -30,7 +48,8 @@ const switchPlayer = function(){
     //change the background color to the active player 
         player0El.classList.toggle('player--active');
         player1El.classList.toggle('player--active');
-}
+};
+init();
 
 //rolling the dice functionallity 
 
@@ -52,10 +71,9 @@ btnRoll.addEventListener('click' , function(){
          
     } else{
         switchPlayer();
-
     }
  }
-})
+});
 
 btnHold.addEventListener('click', function(){
     if(playing){
@@ -75,11 +93,9 @@ btnHold.addEventListener('click', function(){
 
 
     }
-
-
-    
-
-}
-
+ }
 });
 
+//setting new game functionallity 
+
+btnNew.addEventListener('click', init);
